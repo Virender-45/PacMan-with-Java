493c073f9f714844091c0f7a141d8b2bdd34a08f
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-19 20:03:17 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,25 @@ import javax.swing.JFrame; //For window
 
 public class Main {
     public static void main(String[] args) {
-        
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        //Create and initialize a Window
+        JFrame frame = new JFrame("PacMan");
+//        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
+
     }
 }
